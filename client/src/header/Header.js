9635641be2a8a7604315934ff0e65f02a71f0669
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-12-17 15:27:16 -0700

    Added links array and props to navigation.

--- client/src/header/Header.js
+++ client/src/header/Header.js
@@ -3,10 +3,12 @@ import "./Header.scss";
 import Navigation from "../components/navigation/Navigation";
 
 function Header() {
+    const links = [ { name: "Home", href: "/" }, { name: "About", href: "/about" }, { name: "Login", href: "/login" }, { name: "Sign Up", href: "/signup" } ];
+
     return (
         <header>
             <img alt="Logo" src="#"/>
-            {/* <Navigation /> */}
+            <Navigation links={links}/>
         </header>
     );
 }
